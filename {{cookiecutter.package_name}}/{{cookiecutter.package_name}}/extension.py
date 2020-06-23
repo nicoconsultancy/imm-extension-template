fd93e2fc133511fdc46d06a60f4e9905633dde8c
Chris R <cdrx@users.noreply.github.com>
2020-06-23 12:29:09 +0100

    Update extension.py

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/extension.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/extension.py
@@ -4,6 +4,6 @@ from imm.core.extension import Extension
 extension = Extension()
 
 
-@extension.view('/')
+@extension.view(path='/')
 def index(request):
     return render(request, '{{cookiecutter.package_name}}/hello_world.html')
