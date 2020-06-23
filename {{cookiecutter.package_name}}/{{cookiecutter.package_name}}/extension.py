d705b0c3ae3d1effa2c961aeecfd8cfb7dfb2cee
Chris R <cdrx@users.noreply.github.com>
2020-06-23 12:29:01 +0100

    Update extension.py

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/extension.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/extension.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
 from imm.core.extension import Extension
 
-extension = Extension(name="{{cookiecutter.package_name}}")
-
-
-@extension.ready
-def ready():
-    extension.log.info("Extension {{cookiecutter.package_name}} is running")
+extension = Extension()
 
 
 @extension.view('/')
