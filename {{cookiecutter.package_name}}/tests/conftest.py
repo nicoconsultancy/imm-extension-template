005051fc99682397504577b5f447d7960501e996
Chris R <cdrx@users.noreply.github.com>
2021-02-18 13:01:02 +0000

    tests: make sure the default test passes pytest linting

--- {{cookiecutter.package_name}}/tests/conftest.py
+++ {{cookiecutter.package_name}}/tests/conftest.py
@@ -2,6 +2,6 @@ import pytest
 
 
 @pytest.fixture(autouse=True)
-def enable_db_access_for_all_tests(db):
+def _enable_db_access_for_all_tests(db):
     # enough of the tests use the database that we can enable it for all of them
     pass
